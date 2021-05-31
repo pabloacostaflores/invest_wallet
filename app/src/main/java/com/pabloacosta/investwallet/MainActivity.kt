0f8dcda24aa30f628fd415147c5adf0d813fbb04
Pablo Acosta Flores <pabloacosta19@icloud.com>
2021-05-30 23:22:32 -0500

    Initial commit, The login layout looks good and the app works.

---
+++ app/src/main/java/com/pabloacosta/investwallet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pabloacosta.investwallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
