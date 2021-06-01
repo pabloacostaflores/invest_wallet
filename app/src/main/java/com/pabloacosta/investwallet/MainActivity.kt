79ec6324b32fc47081824803a490b1b9a1c91d27
Pablo Acosta Flores <pabloacosta19@icloud.com>
2021-05-31 22:34:51 -0500

    Added the "Register Activity" with all the needed camps and the "Register" button of the Main Activity redirects to the "Register Activity", all works fine.

--- app/src/main/java/com/pabloacosta/investwallet/MainActivity.kt
+++ app/src/main/java/com/pabloacosta/investwallet/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.pabloacosta.investwallet
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val registerButton = findViewById<Button>(R.id.registerButton)
+        registerButton.setOnClickListener {
+            val intent:Intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
