6dbf48173082753276604275d6c08b05ec0b335f
José Emiliano Pérez Garduño <jose.perez@adevinta.com>
2021-06-17 20:58:56 -0500

    intial stock fragment skeleton

---
+++ app/src/main/java/com/pabloacosta/investwallet/StockFragment.kt
@@ -0,0 +1,23 @@
+package com.pabloacosta.investwallet
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class StockFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_stock_fragment, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
