9ff1d9a0e06ed916405a45e8236e17aecb52bdc5
José Emiliano Pérez Garduño <jose.perez@adevinta.com>
2021-06-17 21:25:29 -0500

    logos and a few details when refactoring code, functions TODO and crypto fragment

---
+++ app/src/main/java/com/pabloacosta/investwallet/CryptoFragment.kt
@@ -0,0 +1,7 @@
+package com.pabloacosta.investwallet
+
+import androidx.fragment.app.Fragment
+
+class CryptoFragment : Fragment(R.layout.fragment_stock) {
+
+}
--- app/src/main/java/com/pabloacosta/investwallet/StockFragment.kt
+++ app/src/main/java/com/pabloacosta/investwallet/StockFragment.kt
@@ -1,23 +1,13 @@
 package com.pabloacosta.investwallet
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
-class StockFragment : Fragment() {
+class StockFragment : Fragment(R.layout.fragment_stock) {
+    /*
+    TODO:
+    monto taza y plazo para ir haciendo el calculo de lo que vaya a ganar
+    el valor final queda en una base de datos
+    */
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stock_fragment, container, false)
-    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 }
