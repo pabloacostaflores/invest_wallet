6b51517ad36f6d00f2147823718e0eba1b348d86
Pablo Acosta Flores <pabloacosta19@icloud.com>
2021-06-23 13:35:45 -0500

    Trying to fix it

--- app/src/main/java/com/pabloacosta/investwallet/data/Inversion.kt
+++
@@ -1,7 +0,0 @@
-package com.pabloacosta.investwallet.data
-
-data class Inversion(
-    val name : String,
-    val valor : String,
-    val img : Int
-)
--- app/src/main/java/com/pabloacosta/investwallet/data/PagosUser.kt
+++
@@ -1,6 +0,0 @@
-package com.pabloacosta.investwallet.data
-
-data class PagosUser(
-    val monto: Float,
-    val typeInversion : Int
-)
